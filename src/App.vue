1479f49d1a50a30300f074242d4831e949c2f5df
Miroslav Matkovic <mikki021@gmail.com>
2020-03-08 23:54:51 +0100

    toolbar added

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,102 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item v-for="(menuItem, index) in menuItems" :key="index" link>
+          <v-list-item-action>
+            <v-icon>{{ menuItem.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ menuItem.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar app color="light-blue darken-3" dark>
+      <v-app-bar-nav-icon
+        class="d-flex d-sm-none"
+        @click.stop="drawer = !drawer"
+      />
+      <v-toolbar-title>Event app</v-toolbar-title>
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+        v-for="(menuItem, index) in menuItems"
+        :key="index"
         text
+        class="d-none d-sm-flex"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <v-icon left>{{ menuItem.icon }}</v-icon>
+        {{ menuItem.title }}
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <main>
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col class="text-center">
+              <v-tooltip left>
+                <template v-slot:activator="{ on }">
+                  <v-btn :href="source" icon large target="_blank" v-on="on">
+                    <v-icon large>mdi-code-tags</v-icon>
+                  </v-btn>
+                </template>
+                <span>Source</span>
+              </v-tooltip>
+
+              <v-tooltip right>
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    icon
+                    large
+                    href="https://codepen.io/johnjleider/pen/zgxeLQ"
+                    target="_blank"
+                    v-on="on"
+                  >
+                    <v-icon large>mdi-codepen</v-icon>
+                  </v-btn>
+                </template>
+                <span>Codepen</span>
+              </v-tooltip>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </main>
+    <v-footer color="light-blue darken-3" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
+  components: {},
+
+  props: {
+    source: String
   },
 
   data: () => ({
-    //
+    drawer: false,
+    menuItems: {
+      events: {
+        icon: "mdi-calendar",
+        title: "View Events"
+      },
+      new: {
+        icon: "mdi-calendar-plus",
+        title: "Add Event"
+      },
+      profile: {
+        icon: "mdi-account",
+        title: "Profile"
+      }
+    }
   })
 };
 </script>
